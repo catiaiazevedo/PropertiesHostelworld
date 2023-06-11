b5f68bacf24bfa6f19bbe919f0f4517b8094937e
Catia Azevedo <catiaiazevedo@ua.pt>
2023-06-11 14:04:15 +0100

    Created new packages for better code organization

--- app/src/main/java/com/example/propertieshostelworld/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/propertieshostelworld/ui/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.propertieshostelworld
+package com.example.propertieshostelworld.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.propertieshostelworld.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
